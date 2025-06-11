67ead71b9f543629c4947b874e5e8e3e7e1c4998
ZAID <104031947+zaid692@users.noreply.github.com>
2025-06-11 16:43:54 +0500

    Create mongo.ts

---
+++ lib/mongo.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URL as string;
+
+if (!uri) {
+  throw new Error("Please define the MONGO_URL environment variable inside .env.local");
+}
+
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable to preserve the value across module reloads caused by HMR (Hot Module Replacement).
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
