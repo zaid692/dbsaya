86a508ba9a87ca03bf5db5632aa5828e071294b2
ZAID <104031947+zaid692@users.noreply.github.com>
2025-06-11 13:05:32 +0500

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import ModalPopup from './card2';
 import { PhoneCallIcon, Mail, SquarePlay, SquareChartGantt, VideoIcon } from 'lucide-react';
